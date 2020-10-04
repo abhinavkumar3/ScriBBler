1e7c6dd9a00b8723199285ac029d05b0205a4514
Abhinav <abhinav.kumar3@gmail.com>
2020-10-04 09:54:37 +0530

    Post page are added

---
+++ scripts/postslist.js
@@ -0,0 +1,13 @@
+function ShowTrashModal() {
+    var modal = document.getElementById("modal-trash");
+    modal.style.display = "flex";
+}
+
+function closeContainer() {
+    var modal = document.getElementById("modal-trash");
+    modal.style.display = "None";
+}
+
+function navigateToPost() {
+    location.href = "post.html";
+}
